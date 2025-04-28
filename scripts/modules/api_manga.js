5ad711ad25164c17f5981815df077e407fb70dff
jack-scarlet <82548176+jack-scarlet@users.noreply.github.com>
2025-04-27 22:32:09 -0300

    Create api_manga.js

---
+++ scripts/modules/api_manga.js
@@ -0,0 +1,21 @@
+// scripts/modules/api.js
+export async function fetchAnimeData() {
+  try {
+    const response = await fetch('../list/anime_list.json?_=' + Date.now());
+    if (!response.ok) throw new Error("HTTP error: " + response.status);
+    
+    const data = await response.json();
+    
+    // Converte o objeto de categorias em array único
+    const allAnimes = Object.values(data).flat();
+    
+    if (!Array.isArray(allAnimes)) {
+      throw new Error("Formato inválido após conversão");
+    }
+    
+    return allAnimes;
+  } catch (error) {
+    console.error("Falha ao carregar animes:", error);
+    return [];
+  }
+}
