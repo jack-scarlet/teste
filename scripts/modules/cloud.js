e5ad089c6e0814f3b4df3eff7a8f22e11f76faf6
jack-scarlet <82548176+jack-scarlet@users.noreply.github.com>
2025-04-28 22:50:19 -0300

    Update cloud.js

--- scripts/modules/cloud.js
+++ scripts/modules/cloud.js
@@ -51,9 +51,8 @@ export const initCloudButton = () => {
         const promptHTML = `
             <div class="cloud-prompt">
                 <div class="prompt-header">
-                <button class="close-prompt" aria-label="Fechar">
+                    <button class="close-prompt" aria-label="Fechar">
                     <h3>Configurar Link da Nuvem</h3>
-                   
                         <i class="fas fa-times"></i>
                     </button>
                 </div>
